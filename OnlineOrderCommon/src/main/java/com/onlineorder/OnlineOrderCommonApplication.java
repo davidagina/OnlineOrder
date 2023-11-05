2244cabce63a7f781ec91c9fe43b81fa1259a89f
davidagina <davidagina64@gmail.com>
2023-11-05 04:29:43 +0100

    OnlineOrder Common Module initial commit

---
+++ OnlineOrderCommon/src/main/java/com/onlineorder/OnlineOrderCommonApplication.java
@@ -0,0 +1,13 @@
+package com.onlineorder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineOrderCommonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlineOrderCommonApplication.class, args);
+	}
+
+}
