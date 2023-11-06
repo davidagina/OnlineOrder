e7c37035615f6504ad9df3b69ca20e42b27604d9
davidagina <davidagina64@gmail.com>
2023-11-06 22:07:06 +0100

    Added OnlineOrder BackEnd module

---
+++ OnlineOrderWebParent/OnlineOrderBackEnd/src/main/java/com/onlineorder/OnlineOrderBackEndApplication.java
@@ -0,0 +1,13 @@
+package com.onlineorder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineOrderBackEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlineOrderBackEndApplication.class, args);
+	}
+
+}
